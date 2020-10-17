ad2136837f6006417da0b3f19d781ebf276f5628
benvaro <benvaro91@gmail.com>
2020-10-17 13:23:36 +0300

    components classes, state

--- src/component/card-list/card-list.js
+++ src/component/card-list/card-list.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Card from '../card';
 import './card-list.css'
 
-const CardList = () => {
-    return <div className="row">
-        <Card  />
-        <Card  />
-        <Card  />
-    </div>
-}
 
-export default CardList;
+
+export default class CardList extends Component {
+    state = {
+        obj: [{
+            id: 1,
+            name: "me before you",
+            year: 2015,
+            genre: "drama"
+        },
+        {
+            id: 2,
+            name: "1+1",
+            year: 2020,
+            genre: "psychology"
+        },
+        {
+            id: 3,
+            name: "dark knight",
+            year: 2019,
+            genre: "fantasy"
+        },
+        {
+            id: 4,
+            name: "dark",
+            year: 2019,
+            genre: "fantasy"
+        }
+        ]
+    }
+
+    deleteFilm = (id) => {
+        console.log(id);
+        // todo: delete element from array by id
+        // пам'ятаємо: НЕ МОЖНА змінювати state "напряму" - лише через setState
+    }
+
+    render() {
+        return <div className="row">
+            {this.state.obj.map((elem) => { return <Card onDelete={this.deleteFilm} key={elem.name} film={elem} /> })}
+        </div>
+    }
+}
--- src/component/card/card.js
+++ src/component/card/card.js
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './card.css'
 
-const Card = () => {
-    const src = "https://robohash.org/jkhjkljhjj.png";
+export default class Card extends Component {
+    state = {
+        important: false,
+        deleted: false
+    }
 
-    return <div className="col-10 col-md-6 col-lg-4 card">
-        <h2>Avatar</h2>
-        <img src={src} />
-        <div>
-            <p><b>Year:</b>2012</p>
-            <p><b>Genre:</b>Fantazy</p>
+
+    showName = () => {
+        this.setState({ important: !this.state.important });
+        // this.state.important = !this.state.important;
+        console.log("click: " + this.state.important);
+    }
+
+    disableItem = () => {
+        this.setState({ deleted: !this.state.deleted })
+    }
+
+    render() {
+        const { onDelete } = this.props;
+        const { film } = this.props;
+        const { id, name, year, genre } = film;
+        const { important, deleted } = this.state;
+        let className = "col-12 col-md-6 col-lg-4 card";
+        if (important)
+            className += ' important';
+        if (deleted)
+            className += ' disabled';
+        const src = "https://robohash.org/" + name + ".png";
+        return <div className={className} onClick={this.showName}>
+            <div className="d-flex justify-content-between">
+                <h2>{name}​​</h2>
+                <a>
+                    <i className="fas fa-window-close" onClick={() => onDelete(id)}></i>
+                </a>
+            </div>
+
+            <img src={src} />
+            <div>
+                <p><b>Year: </b>{year}​​</p>
+                <p><b>Genre: </b>{genre}​​</p>
+            </div>
         </div>
-    </div>
+    }
 }
-
-export default Card;
